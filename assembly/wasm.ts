0d820a1ac90a642d45d22740c46966f734a075a9
StumbDev <stumbledeveloper@hotmail.com>
2025-04-12 09:08:04 -0300

    Add somethings

--- assembly/wasm.ts
+++ assembly/wasm.ts
@@ -1,4 +1,4 @@
 /**the OhMyAsmWasmHelper function is the helper function for the assembly language  */
 export default function OhMyAsmWasmHelper(name: string, funcName: string, byteType: string): void {
-    //
+    const u32fn = byteType;
 }
